5dea86e69d225f1255951a5d42dd0499020fd3e5
Yujin <tkfkd7038@naver.com>
2024-10-13 02:03:51 +0900

    Comment: theme_provider 주석 추가

--- lib/themes/theme_provider.dart
+++ lib/themes/theme_provider.dart
@@ -6,16 +6,21 @@ class ThemeProvider with ChangeNotifier {
   // initially, set it as light mode
   ThemeData _themeData = lightMode;
 
+  // get the current theme
   ThemeData get themeData => _themeData;
 
+  // is it dark mode currently?
   bool get isDarkMode => _themeData == darkMode;
 
+  // set the theme
   set themeData(ThemeData themeData) {
     _themeData = themeData;
 
+    // update UI
     notifyListeners();
   }
 
+  // toggle between dark & light mode
   void toggleTheme() {
     if (_themeData == lightMode) {
       themeData = darkMode;
