e45eb0afb6c29ac035f9125debf2ea8ad42422a0
Yujin <tkfkd7038@naver.com>
2024-10-14 23:01:30 +0900

    feat: 텍스트 폼 위젯 생성

---
+++ lib/components/my_text_field.dart
@@ -0,0 +1,37 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+class MyTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final String hintText;
+  final bool obscureText;
+  const MyTextField({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    required this.obscureText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(
+        hintText: hintText,
+        // Border when unselected
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Theme.of(context).colorScheme.tertiary),
+          borderRadius: BorderRadius.circular(12),
+        ),
+        // Border when selected
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Theme.of(context).colorScheme.tertiary),
+          borderRadius: BorderRadius.circular(12),
+        ),
+        fillColor: Theme.of(context).colorScheme.secondary,
+        filled: true,
+      ),
+      obscureText: obscureText,
+    );
+  }
+}
