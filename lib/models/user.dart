2c60d3e72ee64bfa5c4022f3b029a29b03dd8b6d
Yujin <tkfkd7038@naver.com>
2024-10-16 16:09:32 +0900

    feat: UserProfile 클래스 생성
    firebase값을 app에서 쓸 수 있게 전환,
    app 값을 firebase에 저장할 수 있게 전환

---
+++ lib/models/user.dart
@@ -0,0 +1,40 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class UserProfile {
+  final String uid;
+  final String name;
+  final String email;
+  final String username;
+  final String bio;
+
+  UserProfile({
+    required this.uid,
+    required this.name,
+    required this.email,
+    required this.username,
+    required this.bio,
+  });
+
+  // firebase -> app
+  factory UserProfile.fromDocument(DocumentSnapshot doc) {
+    return UserProfile(
+      uid: doc['uid'],
+      name: doc['name'],
+      email: doc['email'],
+      username: doc['username'],
+      bio: doc['bio'],
+    );
+  }
+
+  // app -> firebase
+  Map<String, dynamic> toMap() {
+    return {
+      'uid': uid,
+      'name': name,
+      'email': email,
+      'username': username,
+      'bio': bio,
+    };
+  }
+}
