7efd5d589600c45c7180c1f7f85a2c0c7dcbcf22
Yujin <tkfkd7038@naver.com>
2024-10-16 16:05:10 +0900

    feat(ProfilePage): 사용자 이름과 bio 등 정보 보여주는 프로필 페이지

---
+++ lib/pages/profile_page.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
+import 'package:twitter_clone_app/components/my_bio_box.dart';
+import 'package:twitter_clone_app/components/my_input_alert_box.dart';
+import 'package:twitter_clone_app/models/user.dart';
+import 'package:twitter_clone_app/services/auth/auth_service.dart';
+import 'package:twitter_clone_app/services/database/database_provider.dart';
+
+class ProfilePage extends StatefulWidget {
+  final String uid;
+
+  const ProfilePage({
+    super.key,
+    required this.uid,
+  });
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  // providers
+  late final databaseProvider =
+      Provider.of<DatabaseProvider>(context, listen: false);
+
+  // user info
+  UserProfile? user;
+  String currentUserId = AuthService().getCurrentUid();
+
+  final bioTextController = TextEditingController();
+
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    loadUser();
+  }
+
+  Future<void> loadUser() async {
+    user = await databaseProvider.userProfile(widget.uid);
+
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
+  void _showEditBioBox() {
+    showDialog(
+        context: context,
+        builder: (context) => MyInputAlertBox(
+              textController: bioTextController,
+              hintText: 'Edit bio..',
+              onPressed: saveBio,
+              onPressedText: "Save",
+            ));
+  }
+
+  Future<void> saveBio() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    // update bio
+    await databaseProvider.updateBio(bioTextController.text);
+
+    await loadUser();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+            _isLoading ? '' : user!.name,
+          ),
+          foregroundColor: Theme.of(context).colorScheme.primary,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25),
+          child: ListView(
+            children: [
+              //username handle
+              Center(
+                child: Text(
+                  _isLoading ? '' : '@${user!.username}',
+                  style:
+                      TextStyle(color: Theme.of(context).colorScheme.primary),
+                ),
+              ),
+              const SizedBox(height: 25),
+
+              // profile picture
+              Center(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.secondary,
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                  padding: const EdgeInsets.all(25),
+                  child: Icon(
+                    Icons.person,
+                    size: 72,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 25),
+              // profile states
+
+              // follow / unfollow button
+
+              // edit bio
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'bio',
+                    style:
+                        TextStyle(color: Theme.of(context).colorScheme.primary),
+                  ),
+                  GestureDetector(
+                    onTap: _showEditBioBox,
+                    child: Icon(
+                      Icons.settings,
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10),
+              // bio box
+              MyBioBox(text: _isLoading ? '...' : user!.bio),
+              // list of posts from user
+            ],
+          ),
+        ));
+  }
+}
