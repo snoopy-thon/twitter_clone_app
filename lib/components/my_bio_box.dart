1af6fdb3fb085f15cbee3f6014cc017d5d081829
Yujin <tkfkd7038@naver.com>
2024-10-16 16:05:59 +0900

    feat(MyBioBox): Bio 정보 보여주는 컨테이너

---
+++ lib/components/my_bio_box.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class MyBioBox extends StatelessWidget {
+  final String text;
+  const MyBioBox({
+    super.key,
+    required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        color: Theme.of(context).colorScheme.secondary,
+      ),
+      padding: const EdgeInsets.all(25),
+      child: Text(
+        text.isNotEmpty ? text : "Empty bio..",
+        style: TextStyle(color: Theme.of(context).colorScheme.inversePrimary),
+      ),
+    );
+  }
+}
