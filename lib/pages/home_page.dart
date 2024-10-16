1b3ab1f4902d03133169cb065d0fee392e2b348a
Yujin <tkfkd7038@naver.com>
2024-10-16 17:53:52 +0900

    feat(HomePage): 포스트한 게시물 홈페이지에서 리스트형식으로 보여주기

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:twitter_clone_app/components/my_drawer.dart';
+import 'package:twitter_clone_app/components/my_input_alert_box.dart';
+import 'package:twitter_clone_app/components/my_post_tile.dart';
+import 'package:twitter_clone_app/services/database/database_provider.dart';
+
+import '../models/post.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,16 +15,69 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late final databaseProvider =
+      Provider.of<DatabaseProvider>(context, listen: false);
+
+  late final listeningProvider = Provider.of<DatabaseProvider>(context);
+  final _messageController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    loadAllPosts();
+  }
+
+  Future<void> loadAllPosts() async {
+    await databaseProvider.loadAllPosts();
+  }
+
+  void _openPostMessageBox() {
+    showDialog(
+      context: context,
+      builder: (context) => MyInputAlertBox(
+          textController: _messageController,
+          hintText: "What's on your mind?",
+          onPressed: () async {
+            await postMessage(_messageController.text);
+          },
+          onPressedText: "Post"),
+    );
+  }
+
+  Future<void> postMessage(String message) async {
+    await databaseProvider.postMessage(message);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
-      drawer: const MyDrawer(),
+      drawer: MyDrawer(),
       appBar: AppBar(
         title: const Text('H O M E'),
         centerTitle: true,
         foregroundColor: Theme.of(context).colorScheme.primary,
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _openPostMessageBox,
+        child: const Icon(Icons.add),
+      ),
+      body: _buildPostList(listeningProvider.allPosts),
     );
+  }
+
+  Widget _buildPostList(List<Post> posts) {
+    return posts.isEmpty
+        ? const Center(
+            child: Text("Nothing here.."),
+          )
+        : ListView.builder(
+            itemCount: posts.length,
+            itemBuilder: (context, index) {
+              final post = posts[index];
+
+              return MyPostTile(post: post);
+            });
   }
 }
