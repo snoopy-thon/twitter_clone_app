e3d4c6fe970a60932d23eaae073fb3baf57bb925
Yujin <tkfkd7038@naver.com>
2024-10-16 13:37:22 +0900

    feat(DatabaseService): Firebase에서 user정보 가져오기, Firebase에 user bio 업데이트

--- lib/services/database/database_service.dart
+++ lib/services/database/database_service.dart
@@ -2,10 +2,14 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:twitter_clone_app/models/user.dart';
 
+import '../auth/auth_service.dart';
+
 class DatabaseService {
   final _db = FirebaseFirestore.instance;
   final _auth = FirebaseAuth.instance;
 
+  // USER PROFILE
+  // Save user Info
   Future<void> saveUserInfoFirebase(
       {required String name, required String email}) async {
     String uid = _auth.currentUser!.uid;
@@ -24,4 +28,33 @@ class DatabaseService {
     // save user Info in firebase
     await _db.collection("Users").doc(uid).set(userMap);
   }
+
+  // Get user Info
+  Future<UserProfile?> getUserFromFirebase(String uid) async {
+    try {
+      // retrieve user doc from firebase
+      DocumentSnapshot userDoc = await _db.collection("Users").doc(uid).get();
+
+      // convert doc to user profile
+      return UserProfile.fromDocument(userDoc);
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
+
+  // Update user bio
+  Future<void> updateUserBioFirebase(String bio) async {
+    String uid = AuthService().getCurrentUid();
+    try {
+      await _db.collection("Users").doc(uid).update({'bio': bio});
+    } catch (e) {
+      print(e);
+    }
+  }
+  // POST MESSAGE
+
+  // LIKES
+
+  // FOLLOW
 }
