eaf8af2e6a046ed2f316f1679a6c4b5d686a6b3a
Yujin <tkfkd7038@naver.com>
2024-10-16 17:52:34 +0900

    feat: post한 message 상태관리

--- lib/services/database/database_provider.dart
+++ lib/services/database/database_provider.dart
@@ -2,6 +2,8 @@ import 'package:flutter/foundation.dart';
 import 'package:twitter_clone_app/models/user.dart';
 import 'package:twitter_clone_app/services/database/database_service.dart';
 
+import '../../models/post.dart';
+
 class DatabaseProvider extends ChangeNotifier {
   final _db = DatabaseService();
 
@@ -10,4 +12,27 @@ class DatabaseProvider extends ChangeNotifier {
 
   // update user bio
   Future<void> updateBio(String bio) => _db.updateUserBioFirebase(bio);
+
+  // POSTS
+  // local list of posts
+  List<Post> _allPosts = [];
+
+  // get post
+  List<Post> get allPosts => _allPosts;
+
+  // post message
+  Future<void> postMessage(String message) async {
+    await _db.postMessageInFirebase(message);
+
+    // reload data from firebase
+    await loadAllPosts();
+  }
+
+  Future<void> loadAllPosts() async {
+    final allPosts = await _db.getAllPostsFromFirebase();
+
+    _allPosts = allPosts;
+
+    notifyListeners();
+  }
 }
