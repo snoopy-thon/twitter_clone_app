cb50657517526e7040fe46615e5c736f966fb49d
Yujin <tkfkd7038@naver.com>
2024-10-16 21:17:43 +0900

    feat(MyPostTile): MyPostTile 자체를 GestureDector로 변경
    게시글 누르면 PostPage로 갈 수 있도록 설정

--- lib/components/my_post_tile.dart
+++ lib/components/my_post_tile.dart
@@ -6,9 +6,13 @@ import '../models/post.dart';
 
 class MyPostTile extends StatefulWidget {
   final Post post;
+  final void Function()? onUserTap;
+  final void Function()? onPostTap;
   const MyPostTile({
     super.key,
     required this.post,
+    required this.onUserTap,
+    required this.onPostTap,
   });
 
   @override
@@ -18,48 +22,51 @@ class MyPostTile extends StatefulWidget {
 class _MyPostTileState extends State<MyPostTile> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
-      padding: const EdgeInsets.all(20),
-      decoration: BoxDecoration(
-        color: Theme.of(context).colorScheme.secondary,
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
+    return GestureDetector(
+      onTap: widget.onPostTap,
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 25, vertical: 5),
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.secondary,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
             GestureDetector(
               onTap: widget.onUserTap,
               child: Row(
-            children: [
-              Icon(
-                Icons.person,
-                color: Theme.of(context).colorScheme.primary,
+                children: [
+                  Icon(
+                    Icons.person,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                  const SizedBox(width: 10),
+                  Text(
+                    widget.post.name,
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+                  ),
+                  const SizedBox(width: 4),
+                  Text(
+                    '@${widget.post.username}',
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+                  ),
+                ],
               ),
-              const SizedBox(width: 10),
-              Text(
-                widget.post.name,
-                style: TextStyle(
-                  color: Theme.of(context).colorScheme.primary,
-                ),
-              ),
-              const SizedBox(width: 4),
-              Text(
-                '@${widget.post.username}',
-                style: TextStyle(
-                  color: Theme.of(context).colorScheme.primary,
-                ),
-              ),
-            ],
-              ),
-          ),
-          const SizedBox(height: 20),
-          Text(
-            widget.post.message,
+            ),
+            const SizedBox(height: 20),
+            Text(
+              widget.post.message,
               style: TextStyle(
                   color: Theme.of(context).colorScheme.inversePrimary),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
