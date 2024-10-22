e7b8d41fd47e440029606c5617422f720a81fbb0
Yujin <tkfkd7038@naver.com>
2024-10-22 21:46:48 +0900

    feat: 로컬에서 '좋아요' 관련 로직 구현
    '좋아요' 로컬 UI 업데이트 및 파이어베이스에서 업데이트

--- lib/services/database/database_provider.dart
+++ lib/services/database/database_provider.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/foundation.dart';
 import 'package:twitter_clone_app/models/user.dart';
+import 'package:twitter_clone_app/services/auth/auth_service.dart';
 import 'package:twitter_clone_app/services/database/database_service.dart';
 
+import '../../models/comment.dart';
 import '../../models/post.dart';
 
 class DatabaseProvider extends ChangeNotifier {
   final _db = DatabaseService();
+  final _auth = AuthService();
 
   // get user profile given uid
   Future<UserProfile?> userProfile(String uid) => _db.getUserFromFirebase(uid);
@@ -31,8 +34,13 @@ class DatabaseProvider extends ChangeNotifier {
   Future<void> loadAllPosts() async {
     final allPosts = await _db.getAllPostsFromFirebase();
 
+    // update local data
     _allPosts = allPosts;
 
+    // initalize local like data
+    initializeLikeMap();
+
+    // update UI
     notifyListeners();
   }
 
@@ -47,4 +55,64 @@ class DatabaseProvider extends ChangeNotifier {
   }
 
   // Likes
+  // Local map to track like counts for each post
+  Map<String, int> _likeCounts = {};
+
+  // Local list to track posts liked by current user
+  List<String> _likedPosts = [];
+
+  bool isPostLikedByCurrentUser(String postId) => _likedPosts.contains(postId);
+
+  int getLikeCount(String postId) => _likeCounts[postId] ?? 0;
+
+  // initialize like map locally
+  void initializeLikeMap() {
+    // get current uid
+    final currentUserID = _auth.getCurrentUid();
+
+    // 새로운 유저가 로그인 했을 때, 로컬 데이터 clear
+    _likedPosts.clear();
+
+    // for each post get like data
+    for (var post in _allPosts) {
+      // update like count map
+      _likeCounts[post.id] = post.likeCount;
+
+      // if the current user already likes this post
+      if (post.likedBy.contains(currentUserID)) {
+        // add this post id to local list of liked posts
+        _likedPosts.add(post.id);
+      }
+    }
+  }
+
+// toggle like
+  Future<void> toggleLike(String postId) async {
+    // database에 접근하는데 시간이 걸리니, User 에게 빠르게 보여주기 위해서 db접근 전에 UI를 먼저 업데이트 해주기
+
+    // DB 저장에 실패했을때를 대비하여 원래 값 저장하기
+    final likedPostsOriginal = _likedPosts;
+    final likeCountsOriginal = _likeCounts;
+
+    if (_likedPosts.contains(postId)) {
+      _likedPosts.remove(postId);
+      _likeCounts[postId] = (_likeCounts[postId] ?? 0) - 1;
+    } else {
+      _likedPosts.add(postId);
+      _likeCounts[postId] = (_likeCounts[postId] ?? 0) + 1;
+    }
+    // 로컬에서 UI 업데이트
+    notifyListeners();
+
+    // 데이터베이스에서 업데이트하기
+    try {
+      await _db.toggleLikeInFirebase(postId);
+    } catch (e) {
+      // DB 저장 실패했을경우 원래대로 되돌리기
+      _likedPosts = likedPostsOriginal;
+      _likeCounts = likeCountsOriginal;
+
+      notifyListeners();
+    }
+  }
 }
